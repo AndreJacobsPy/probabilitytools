b2ca5167d6dad928a8ffe0775d935ca2f2104a42
andrejacobspy <andrejacobs365@gmail.com>
2024-01-27 13:53:57 +0100

    doc fix

--- R/basics.R
+++ R/basics.R
@@ -3,6 +3,7 @@
 #' It just loops through and prints values based on integer input.
 #'
 #' @param x -> integer
+#' @returns Nothing
 #' @export
 some_function <- function(x) {
   for (i in 1:x) print(i)
