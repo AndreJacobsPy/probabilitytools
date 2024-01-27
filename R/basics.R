39f552218bfb2fc9a995cd225e3527bba7bb221a
andrejacobspy <andrejacobs365@gmail.com>
2024-01-27 13:48:58 +0100

    update

--- R/basics.R
+++ R/basics.R
@@ -1,3 +1,8 @@
+#' This is a test function.
+#'
+#' It just loops through and prints values based on integer input.
+#'
+#' @param x -> integer
 #' @export
 some_function <- function(x) {
   for (i in 1:x) print(i)
