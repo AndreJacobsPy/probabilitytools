a3259ed7075971eb46f228cdbc47ae07c670b317
andrejacobspy <andrejacobs365@gmail.com>
2024-01-28 16:04:58 +0100

    bug fix - documentation

--- R/binomial.R
+++ R/binomial.R
@@ -1,4 +1,4 @@
-#' pr.binomial
+#' pr_binomial
 #'
 #' Function for calculating probability for Bernouli Trials.
 #'
@@ -7,10 +7,10 @@
 #' @param p -> the probability of j
 #' @return probability of Binomial Random Variable
 #' @examples
-#' pr.binomial(10, 2, 1/6)
+#' pr_binomial(10, 2, 1/6)
 #' > 0.29071
 #'
-#' pr.binomial(n = 5, j = 5, p = 1/2)
+#' pr_binomial(n = 5, j = 5, p = 1/2)
 #' > 0.03125
 #'
 #' @export
@@ -19,7 +19,7 @@ pr_binomial <- function(n, j, p) {
   return(combinations * p^j * (1 - p)^(n - j))
 }
 
-#' mean.binomial
+#' mean_binomial
 #'
 #' function that calculates the mean of outcomes, or the
 #' expected value.
@@ -32,13 +32,13 @@ mean_binomial <- function(n, p) {
   return(n * p)
 }
 
-#' var.binomial
+#' var_binomial
 #'
 #' function that calculates the variance of outcomes.
 #'
 #' @param n -> number of trials
 #' @param p -> the probability of success
-#' @return the mean or expected value
+#' @return the variance of trials
 #' @export
 var_binomial <- function(n, p) {
   return(n * p * (1 - p))
